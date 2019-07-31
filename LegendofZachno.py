4d7a375fe57523dc492f9816a45e3dc63d03bee2
onnodoove <48141978+onnodoove@users.noreply.github.com>
2019-07-31 02:45:51 +0200

    Add files via upload
    - Basic display code and movement for player added

--- LegendofZachno.py
+++ LegendofZachno.py
@@ -39,13 +39,99 @@ tan = ((230,220,170))
 coffee_brown =((200,190,140))
 moon_glow = ((235,245,255))
 
-Floor=pygame.image.load('floor.PNG')
-Stairs=pygame.image.load('stairs.PNG')
-Wall=pygame.image.load('wall.PNG')
+Black=pygame.image.load('Black.png')
+Floor=pygame.image.load('newFloor.PNG')
+Stairs=pygame.image.load('newStairs.PNG')
+Wall=pygame.image.load('newWall.PNG')
+Player=pygame.image.load('newMonster.PNG')
 
-pygame.display.set_icon()
+#pygame.display.set_icon()
 pygame.display.set_caption('Legend of Zachno')
-Width=int(600)
-Heigth=int(400)
+Width=int(1200)
+Heigth=int(800)
 screen = pygame.display.set_mode((Width, Heigth))
 
+PlayerX=0
+PlayerY=0
+
+Labyrinth=list()
+VisualList=list()
+
+Labyrinth=['Wall','-3','3','Wall','-2','3','Wall','-1','3','Wall','0','3','Wall','1','3','Wall','2','3','Wall','3','3','Wall','-3','2','Floor','-2','2','Floor','-1','2','Floor','0','2',
+'Floor','1','2','Floor','2','2','Wall','3','2','Wall','-3','1','Floor','-2','1','Floor','-1','1','Floor','0','1','Floor','1','1','Floor','2','1','Wall','3','1',
+'Wall','-3','0','Floor','-2','0','Floor','-1','0','Floor','0','0','Floor','1','0','Floor','2','0','Wall','3','0',
+'Wall','-3','-1','Floor','-2','-1','Floor','-1','-1','Floor','0','-1','Floor','1','-1','Floor','2','-1','Wall','3','-1',
+'Wall','-3','-2','Floor','-2','-2','Floor','-1','-2','Floor','0','-2','Floor','1','-2','Floor','2','-2','Wall','3','-2',
+'Wall','-3','-3','Wall','-2','-3','Wall','-1','-3','Wall','0','-3','Wall','1','-3','Wall','2','-3','Wall','3','-3']
+
+def wait():
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_KP_ENTER or event.key == pygame.K_RETURN:
+	                		return
+
+def VisualScan(Labyrinth):
+	del VisualList[:]
+	MaxCounter=len(Labyrinth)
+	Counter=0
+	while Counter<MaxCounter:
+		Object=str(Labyrinth[Counter])
+		ObjectX=int(Labyrinth[Counter+1])
+		ObjectY=int(Labyrinth[Counter+2])
+		XDiff=ObjectX-PlayerX
+		YDiff=ObjectY-PlayerY
+		if (-7 <= XDiff) and ( XDiff <= 7) and (-4 <= YDiff) and (YDiff <= 4):
+			VisualList.append(Object)
+			VisualList.append(XDiff)
+			VisualList.append(YDiff)
+		Counter=Counter+3
+	return(VisualList)
+
+def GetScreenItem(ObjectImage):
+	if ObjectImage=='Wall':
+		ScreenItem=Wall
+	elif ObjectImage=='Floor':
+		ScreenItem=Floor
+	
+	return(ScreenItem)
+
+def DoScreen (Labyrinth):
+	VisualScan(Labyrinth)
+	Counter=0
+	MaxCounter=len(VisualList)
+	screen.blit(Black,(0,0))
+	while Counter < MaxCounter:
+		ObjectImage=str(VisualList[Counter])
+		ObjectX=int(VisualList[Counter+1])
+		ObjectY=int(VisualList[Counter+2])
+		ScreenItem=GetScreenItem(ObjectImage)
+		ScreenX=(ObjectX+7)*80
+		YConvert=ObjectY*-1
+		ScreenY=(YConvert+4)*80
+		screen.blit(ScreenItem, (ScreenX, ScreenY))
+		Counter=Counter+3
+	screen.blit(Player, (560, 320))
+	pygame.display.flip()
+	return
+
+DoScreen(Labyrinth)
+
+Running=True
+while Running:
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP:
+				PlayerY=PlayerY+1
+			if event.key == pygame.K_RIGHT:
+				PlayerX=PlayerX+1
+			if event.key == pygame.K_DOWN:
+				PlayerY=PlayerY-1
+			if event.key == pygame.K_LEFT:
+				PlayerX=PlayerX-1
+			if event.key == pygame.K_ESCAPE:
+				Running=False
+	DoScreen(Labyrinth)
+
+
+sys.exit()
